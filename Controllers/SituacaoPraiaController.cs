d94be4775d9f86863d985ad98208a63eb8992a4a
LuizFFarias <rm99519@fiap.com.br>
2024-06-07 16:08:33 -0300

    Editar SituacaoPraia

--- Controllers/SituacaoPraiaController.cs
+++ Controllers/SituacaoPraiaController.cs
@@ -1,6 +1,7 @@
 using gs_bluehorizon_dotnet.Models;
 using gs_bluehorizon_dotnet.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace gs_bluehorizon_dotnet.Controllers;
 
@@ -34,5 +35,47 @@ public class SituacaoPraiaController : Controller
         }
 
         return View(situacaoPraia);
+    }
+    
+    public async Task<IActionResult> Editar(long id)
+    {
+        var situacaoPraia = await _repository.FindById(id);
+        if (situacaoPraia == null)
+        {
+            return NotFound();
+        }
+        return View(situacaoPraia);
+    }
+    
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Editar(SituacaoPraia situacaoPraia)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _repository.Update(situacaoPraia);
+                return RedirectToAction("Index", "SituacaoPraia");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SituacaoPraiaExists(situacaoPraia.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        return View(situacaoPraia);
+    }
+
+    private bool SituacaoPraiaExists(long id)
+    {
+        return _repository.FindById(id) != null;
     }
 }
